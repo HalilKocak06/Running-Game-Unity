3aee849264193bd24d2bee7d4a9c79497505424f
Halil İbrahim Koçak <98169696+HalilKocak06@users.noreply.github.com>
2025-11-25 22:54:07 +0300

    V10

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using System;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] ParticleSystem collisionParticleSystem;
+    [SerializeField] AudioSource boulderSmashAudioSource;
+    [SerializeField] float shakeModifer = 10f;
+    [SerializeField] float collisionCooldown = 1f;
+    CinemachineImpulseSource cinemachineImpulseSource;
+
+    float collisionTimer = 0f;
+
+    void Awake()
+    {
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    void Update()
+    {
+        collisionTimer = Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (collisionTimer < collisionCooldown) return;
+
+        FireImpulse();
+        CollisionFX(other);
+        collisionTimer = 0f;
+    }
+
+    private void FireImpulse()
+    {
+        float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        float shakeIntensity = (1f / distance) * shakeModifer;
+        shakeIntensity = Math.Min(shakeIntensity, 1f);
+
+        cinemachineImpulseSource.GenerateImpulse(1f);
+    }
+
+    void CollisionFX(Collision other)
+    {
+        ContactPoint contactPoint = other.contacts[0];
+        collisionParticleSystem.transform.position = contactPoint.point;
+        collisionParticleSystem.Play();
+        boulderSmashAudioSource.Play();
+    }
+}
